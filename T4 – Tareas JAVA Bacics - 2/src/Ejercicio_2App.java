5009ef1a2e775a07f7eeff90f29d64e8a3b04c47
danielmac03-curso <danielmaciasgracia13@gmail.com>
2020-07-15 16:42:04 +0200

    Subido ejercicio 2 de T4 – Tareas JAVA Bacics

---
+++ T4 – Tareas JAVA Bacics - 2/src/Ejercicio_2App.java
@@ -0,0 +1,31 @@
+
+public class Ejercicio_2App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*Escribe un programa Java que realice lo siguiente: declarar una variable N de tipo int,
+		una variable A de tipo double y una variable C de tipo char y asigna a cada una un valor */
+		int n = 10;
+		double a = 5.0;
+		char c = 'A';
+		
+		//El valor de cada variable.
+		System.out.println("El valor de N es igual a: " + n);
+		System.out.println("El valor de A es igual a: " + a);
+		System.out.println("El valor de C es igual a: " + c);
+		
+		//La suma de N + A
+		System.out.println("La suma de N y A: " + (n + a));
+
+		//La diferencia de A - N 
+		System.out.println("La diferencia de N y A: " + (n - a));
+		
+		//El valor numérico correspondiente al carácter que contiene la variable C.
+		int valueC = c;
+		System.out.println("El valor numérico correspondiente al carácter que contiene la variable C es: " + valueC);
+
+		
+	}
+
+}
